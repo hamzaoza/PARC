98697672ef466f0e44fe50f68d8e54ae12339f3b
Hamza Oza <hamza.oza@rca.ac.uk>
2019-03-06 12:00:50 +0000

    Changed disqus identifier

--- src/js/partials/_comments.js
+++ src/js/partials/_comments.js
@@ -1,11 +1,11 @@
 PAD.Comments = function(commentID) {
 
-	var url = window.location.origin + "/#!prod" + commentID;
+	var url = window.location.origin + "/#!demo" + commentID;
 
 	DISQUS.reset({
 		reload: true,
 		config: function () {  
-			this.page.identifier = "prod" + commentID,
+			this.page.identifier = "demo" + commentID,
 			this.page.url = url
 		}
 	});
