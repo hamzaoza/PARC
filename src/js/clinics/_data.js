48eb93c1bdfc1dc3f247ec1236fae4283f69e88e
Hamza Oza <hamza.oza@rca.ac.uk>
2019-06-17 11:32:42 +0100

    Added Site1, Site2 and Skane models

--- src/js/clinics/_data.js
+++ src/js/clinics/_data.js
@@ -1,9 +1,15 @@
 import { Global } from "./_global"
-import { Site3 } from "./_site3"
 import { Pavilion } from "./_pavilion"
+import { Site1 } from "./_site1";
+import { Site2 } from "./_site2";
+import { Site3 } from "./_site3"
+import { Skane } from "./_skane";
 
 export const Data = {
 	"Global" : Global,
+	"Pavilion" : Pavilion,
+	"Site1" : Site1,
+	"Site2" : Site2,
 	"Site3" : Site3,
-	"Pavilion" : Pavilion
+	"Skane" : Skane
 }
---
+++ src/js/clinics/_site1.js
@@ -0,0 +1,14 @@
+export const Site1 = {
+	"models" : [
+		{
+			"name": "floor",
+			"path": "assets/models/site1/site1-floor.json",
+			"castShadow" : false
+		},
+		{
+			"name": "layout",
+			"path": "assets/models/site1/site1-layout.json"
+		}
+	],
+	"hotspots" : []
+};
