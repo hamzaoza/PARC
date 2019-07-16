06883858c136ee2f7f90483d46531cd918062a13
Hamza Oza <hamza.oza@rca.ac.uk>
2019-07-16 16:16:29 +0100

    Update Skane spots

--- src/js/clinics/_skane.js
+++ src/js/clinics/_skane.js
@@ -16,9 +16,14 @@ export const Skane = {
 		"name" : "skane",
 		"position" : [0, 3, 0],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/skane-view.jpg",
 			"title" : "Malmo Infectious Disease Hospital",
 			"position": "left bottom",
+			"theme" : "dark",
+			"credit" : {
+				"text" : "Image courtesy of C.F. Møller Architects",
+				"link" : "https://www.cfmoller.com/p/Emergency-and-Infectious-Diseases-Unit-SUS-Malmo-i2150.html"
+			},
 			"quotes" : [{
 				"quote" : "To better anticipate the coming 25 years, we chose to look back 25 years...Generally a belief prevailed in the medical discourse that problems were solved with antibiotics and by improving socio-economic conditions.",
 				"cite" : "Holmdahl et al (2013)",
@@ -30,9 +35,14 @@ export const Skane = {
 		"name" : "entrance",
 		"position" : [-1.5, 3, 18.5],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/entrance.jpg",
 			"title" : "Entrance",
 			"position": "left bottom",
+			"theme" : "dark",
+			"credit" : {
+				"text" : "Image courtesy of C.F. Møller Architects",
+				"link" : "https://www.cfmoller.com/p/Emergency-and-Infectious-Diseases-Unit-SUS-Malmo-i2150.html"
+			},
 			"quotes" : [{
 				"quote" : "Entrance from the outside makes it possible for patients and relatives to enter rooms under isolation measures without entering the interior areas of the ward.",
 				"cite" : "Holmdahl et al (2013)",
@@ -45,14 +55,13 @@ export const Skane = {
 		"name" : "outer-perimeter",
 		"position" : [11, 3, 16],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/bg-3.jpg",
 			"title" : "Outer Perimeter",
-			"position": "left bottom",
+			"position": "center bottom",
 			"quotes" : [{
 				"quote" : "By proposing a round building, a high degree of standardisation could be achieved because the patient room units were distributed along the outer perimeter...",
 				"cite" : "Holmdahl et al (2013)",
 				"colour" : "grad-pink-blue",
-				"position" : "left middle",
 			}],
 		}
 	},
@@ -60,14 +69,13 @@ export const Skane = {
 		"name" : "circulation",
 		"position" : [-12, 3, 0],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/bg-1.jpg",
 			"title" : "Circulation",
-			"position": "left bottom",
+			"position": "center bottom",
 			"quotes" : [{
 				"quote" : "...it should be possible to separate the flow or circulation of patients with contagious diseases from other patient circulation...",
 				"cite" : "Holmdahl et al (2013)",
 				"colour" : "grad-pink-blue",
-				"position" : "left middle",
 			}],
 		}
 	},
@@ -75,9 +83,14 @@ export const Skane = {
 		"name" : "inner-perimeter",
 		"position" : [-4.3, 3, -8.5],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/corridor.jpg",
 			"title" : "Inner Perimeter",
 			"position": "left bottom",
+			"theme" : "dark",
+			"credit" : {
+				"text" : "Image courtesy of C.F. Møller Architects",
+				"link" : "https://www.cfmoller.com/p/Emergency-and-Infectious-Diseases-Unit-SUS-Malmo-i2150.html"
+			},
 			"quotes" : [{
 				"quote" : "Nursing offices and stations, staff rooms, supply rooms were located in the inner perimeter",
 				"cite" : "Holmdahl et al (2013)",
@@ -90,9 +103,9 @@ export const Skane = {
 		"name" : "microbiome",
 		"position" : [15.2, 3, 4.6],
 		"data" : {
-			"url" : "images/skane/",
+			"url" : "images/skane/bg-2.jpg",
 			"title" : "Human Microbiome",
-			"position": "left bottom",
+			"position": "left top",
 			"quotes" : [{
 				"quote" : "Human microbiome research has shown that the use of antibiotics can disrupt the normal array of microbes that live in and on our bodies. The constant attempts at sterilisation in hospitals might function on a similar level…",
 				"colour" : "pink",
@@ -101,7 +114,7 @@ export const Skane = {
 				"quote" : "…the use of broad-spectrum antibiotics, bleach, and hand sanitiser might take out some of the harmful pathogens, but it also cuts a swath through the hordes of non-pathogenic microorganisms",
 				"cite" : "Carrie. A. (2014)",
 				"colour" : "pink",
-				"position" : "left middle",
+				"position" : "center bottom",
 			}],
 		}
 	}]
